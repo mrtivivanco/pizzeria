bfb4cd2aa77c09e1adc89e8e4a621c9e48b3b786
mane-lab <mane.ulloab@gmail.com>
2025-03-31 20:24:24 -0300

    Update csv.h

--- csv/csv.h
+++ csv/csv.h
@@ -1,8 +1,8 @@
-#ifndef CSV_H
-#define CSV_H
+#ifndef CSV_H //Comprueba si CSV_H no está definido
+#define CSV_H //Si no esta definido, define CSV_H para evitar inclusiones multiples
 
-#include "../ordenes/ordenes.h"
+#include "../ordenes/ordenes.h" //Define la estructura "order"
 
-order* read_csv(const char *filename, int *size);
+order* read_csv(const char *filename, int *size); //Declara la función para leer el CSV
 
-#endif
+#endif //Termina el If de la línea 1
