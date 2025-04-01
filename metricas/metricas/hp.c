7bc23059d6646688a0be26d8b6b01e09de3a094f
Vicho222 <vicemartinez@alumnos.uai.cl>
2025-04-01 00:33:04 -0300

    armar hp

--- metricas/metricas/hp.c
+++ metricas/metricas/hp.c
@@ -1,1 +1,33 @@
+#include "../../ordenes/ordenes.h"
+#include "../metricas.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_CATEGORIES 100
+#define CATEGORY_LENGTH 50
+
+// Cantidad de pizzas por categoría vendidas
+char* hp(int *size, order *orders) {
+    if (size == NULL || orders == NULL || *size <= 0) {
+        return NULL;
+    }
+
+    int unique = 0;
+    char categories[MAX_CATEGORIES][CATEGORY_LENGTH];  // Categorías únicas
+    int categoryCount[MAX_CATEGORIES] = {0};           // Cantidad de pizzas por categoría
+
+    // Contar pizzas por categoría
+    for (int i = 0; i < *size; i++) {
+        char *category = orders[i].pizza_category;
+        int quantity = orders[i].quantity;
+        
+        int found = 0;
+        for (int j = 0; j < unique; j++) {
+            if (strcmp(categories[j], category) == 0) {
+                categoryCount[j] += quantity;
+                found = 1;
+                break;
+            }
+        }
 
