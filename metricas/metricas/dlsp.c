7e83a3c74e4bc05847d88fc8518d4c922b9b8553
Vicho222 <vicemartinez@alumnos.uai.cl>
2025-03-31 21:48:19 -0300

    armar dlsp

--- metricas/metricas/dlsp.c
+++ metricas/metricas/dlsp.c
@@ -1,1 +1,14 @@
+#include "../../ordenes/ordenes.h"
+#include "../metricas.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
+#define MAX_DATES 100
+#define DATE_LENGTH 16
+
+// Fecha con menos ventas en términos de cantidad de pizzas
+char* dlsp(int *size, order *orders) {
+    if (size == NULL || orders == NULL || *size <= 0) {
+        return NULL;
+    }
