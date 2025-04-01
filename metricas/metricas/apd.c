8a0eb971b5286ac642cfe51ebd4da8863d5f43ec
Vicho222 <vicemartinez@alumnos.uai.cl>
2025-03-31 23:55:11 -0300

    apd avance

--- metricas/metricas/apd.c
+++ metricas/metricas/apd.c
@@ -1,1 +1,20 @@
+#include "../../ordenes/ordenes.h"
+#include "../metricas.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
+#define MAX_DATES 100
+#define DATE_LENGTH 16
+
+// Promedio de pizzas por día
+char* apd(int *size, order *orders) {
+    if (size == NULL || orders == NULL || *size <= 0) {
+        return NULL;
+    }
+
+    int unique = 0;
+    char orderDates[MAX_DATES][DATE_LENGTH];  // Fechas únicas
+    int pizzasPerDay[MAX_DATES] = {0};        // Pizzas vendidas por día
+
+    
