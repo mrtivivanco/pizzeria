16badc48345aefc9ea4edfe53e3bb5c371c3b1db
Vicho222 <vicemartinez@alumnos.uai.cl>
2025-03-31 23:19:31 -0300

    armando apo

--- metricas/metricas/apo.c
+++ metricas/metricas/apo.c
@@ -1,1 +1,15 @@
+#include "../../ordenes/ordenes.h"
+#include "../metricas.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+// Promedio de pizzas por orden
+char* apo(int *size, order *orders) {
+    if (size == NULL || orders == NULL || *size <= 0) {
+        return NULL;
+    }
+
+    int totalPizzas = 0;
+    int totalOrders = 0;
+
 
